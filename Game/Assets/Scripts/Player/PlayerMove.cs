eab3879b835c9afe0324700903e59fa0cb411ff7
Christina Subedi <96991580+ChristinaSubedi@users.noreply.github.com>
2024-03-13 14:44:48 +0100

    add left and right movement

--- Game/Assets/Scripts/Player/PlayerMove.cs
+++ Game/Assets/Scripts/Player/PlayerMove.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    public float leftRightSpeed = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Space.world as the movement (run) is relative to the world around it
-        transform.Translate(Vector3.forward *  moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+        //moving left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * -leftRightSpeed);
+        }
     }
 }
