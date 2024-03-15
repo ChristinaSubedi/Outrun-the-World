108f99fe2d8916ec36ac97d2d62634123e72edbf
Christina Subedi <96991580+ChristinaSubedi@users.noreply.github.com>
2024-03-15 14:07:30 +0100

    added fadein and countdown at start

---
+++ Game/Assets/Scripts/Environments/LevelStarter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStarter : MonoBehaviour
+{
+    public GameObject countdown3;
+    public GameObject countdown2;
+    public GameObject countdown1;
+    public GameObject countDownGo;
+    public GameObject fadeIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountSequence());
+    }
+
+    IEnumerator CountSequence()
+    {
+        yield return new WaitForSeconds(1);
+        countdown3.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countdown2.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countdown1.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDownGo.SetActive(true);
+    }
+}
