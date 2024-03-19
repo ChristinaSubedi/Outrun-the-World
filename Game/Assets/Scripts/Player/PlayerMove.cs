bb44eda43a7df9376d26062a41ada08d4c89f494
Christina Subedi <96991580+ChristinaSubedi@users.noreply.github.com>
2024-03-19 03:19:18 +0100

    implement jump

--- Game/Assets/Scripts/Player/PlayerMove.cs
+++ Game/Assets/Scripts/Player/PlayerMove.cs
@@ -7,7 +7,9 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 8f;
     public float leftRightSpeed = 9f;
     static public bool canMove = false;
-
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class PlayerMove : MonoBehaviour
     {
         //Space.world as the movement (run) is relative to the world around it
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
-        if (canMove )
+        if (canMove)
         {
             //moving left
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
@@ -38,6 +40,37 @@ public class PlayerMove : MonoBehaviour
 
                 }
             }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+            {
+                if (isJumping == false)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+            }
+            if (isJumping)
+            {
+                if (comingDown==false)
+                {
+                    transform.Translate(Vector3.up * Time.deltaTime*5, Space.World);
+
+                } else
+                {
+                    transform.Translate(Vector3.down * Time.deltaTime * 5, Space.World);
+                }
+            }
         }
+    }
+
+    IEnumerator JumpSequence()
+    {
+        yield return new WaitForSeconds(0.45f);
+        comingDown = true;
+        yield return new WaitForSeconds(0.45f);
+        isJumping = false;
+        comingDown = false;
+        playerObject.GetComponent<Animator>().Play("Fast Run");
     }
 }
