8c66053c2250093d510a668aaba1da0240bfdb73
Christina Subedi <96991580+ChristinaSubedi@users.noreply.github.com>
2024-03-19 03:41:11 +0100

    deleted unused sections

---
+++ Game/Assets/Scripts/Environments/Destroyer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public string parentName;
+    // Start is called before the first frame update
+ 
+    // Update is called once per frame
+    void Start()
+    {
+        parentName = transform.name;
+        StartCoroutine(DestroyClone());
+
+    }
+
+    IEnumerator DestroyClone()
+    {
+        yield return new WaitForSeconds(30);
+        if (parentName == "Section(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
